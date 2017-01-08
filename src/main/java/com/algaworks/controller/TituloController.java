3a5f1a5e82f537870107eff781b60dd4a88a94e2
Anderson Silva <andersonolisilva@gmail.com>
2017-01-08 17:24:15 -0300

    Implementando a edição dos registros de títulos

--- src/main/java/com/algaworks/controller/TituloController.java
+++ src/main/java/com/algaworks/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,10 +24,12 @@ public class TituloController {
 
 	@Autowired
 	Titulos titulos;
+	
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
 
 	@RequestMapping("/novo")
 	public ModelAndView novo(){
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
@@ -34,7 +37,7 @@ public class TituloController {
 	@RequestMapping(method = RequestMethod.POST)
 	public String salvar(@Validated Titulo titulo, Errors erros, RedirectAttributes attributes){
 		if (erros.hasErrors()){
-			return "CadastroTitulo";
+			return CADASTRO_VIEW;
 		}
 		titulos.save(titulo);
 		attributes.addFlashAttribute("mensagem","Título salvo com sucesso");
@@ -52,6 +55,13 @@ public class TituloController {
 	@ModelAttribute("todosStatusTitulo")
 	public List<StatusTitulo> todosStatusTitulo(){
 		return Arrays.asList(StatusTitulo.values());
+	}
+	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo){
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;		
 	}
 
 }
